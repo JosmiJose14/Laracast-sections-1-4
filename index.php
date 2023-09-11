eea522d898f94fcc807da103650545834e538fcc
Josmi Jose <josmijose100@gmail.com>
2023-09-11 11:42:16 -0700

    PHP router

--- index.php
+++ index.php
@@ -1,38 +1,5 @@
 <?php
-// $books = [
-//     [
-//         'name' => 'Do Androids Dream of Electric Sheep',
-//         'author' => 'author',
-//         'purchaseUrl' => 'http://example.com',
-//         'releaseYear' => 2022
-//     ],
-//     [
-//         'name' => 'Hail Mary',
-//         'author' => 'Andy Weir',
-//         'purchaseUrl' => 'http://example.com',
-//         'releaseYear' => 1928
-//     ],
-//     [
-//         'name' => 'The Martian',
-//         'author' => 'Andy Weir',
-//         'purchaseUrl' => 'http://example.com',
-//         'releaseYear' => 1992
-//     ],
-// ];
-// function  filter($items, $fn)
-// {
-//     $filteredItems = [];
-//     foreach ($items as $item) {
-//         if ($fn($item)) {
-//             $filteredItems[] = $item;
-//         }
-//     }
-//     return $filteredItems;
-// }
-// $filteredBooks = filter($books, function ($book) {
-//     return $book['releaseYear'] >= 2000;
-// });
 
-require "functions.php";
-$heading = "Dashboard";
-require "views/index.view.php";
+require 'functions.php';
+
+require 'router.php';
