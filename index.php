b714c3b802b64f769543de9a1e862f0f44e278e9
Josmi Jose <josmijose100@gmail.com>
2023-09-11 09:35:29 -0700

    Familiarizing page links

--- index.php
+++ index.php
@@ -1,37 +1,38 @@
 <?php
-$books = [
-    [
-        'name' => 'Do Androids Dream of Electric Sheep',
-        'author' => 'author',
-        'purchaseUrl' => 'http://example.com',
-        'releaseYear' => 2022
-    ],
-    [
-        'name' => 'Hail Mary',
-        'author' => 'Andy Weir',
-        'purchaseUrl' => 'http://example.com',
-        'releaseYear' => 1928
-    ],
-    [
-        'name' => 'The Martian',
-        'author' => 'Andy Weir',
-        'purchaseUrl' => 'http://example.com',
-        'releaseYear' => 1992
-    ],
-];
-function  filter($items, $fn)
-{
-    $filteredItems = [];
-    foreach ($items as $item) {
-        if ($fn($item)) {
-            $filteredItems[] = $item;
-        }
-    }
-    return $filteredItems;
-}
-$filteredBooks = filter($books, function ($book) {
-    return $book['releaseYear'] >= 2000;
-});
+// $books = [
+//     [
+//         'name' => 'Do Androids Dream of Electric Sheep',
+//         'author' => 'author',
+//         'purchaseUrl' => 'http://example.com',
+//         'releaseYear' => 2022
+//     ],
+//     [
+//         'name' => 'Hail Mary',
+//         'author' => 'Andy Weir',
+//         'purchaseUrl' => 'http://example.com',
+//         'releaseYear' => 1928
+//     ],
+//     [
+//         'name' => 'The Martian',
+//         'author' => 'Andy Weir',
+//         'purchaseUrl' => 'http://example.com',
+//         'releaseYear' => 1992
+//     ],
+// ];
+// function  filter($items, $fn)
+// {
+//     $filteredItems = [];
+//     foreach ($items as $item) {
+//         if ($fn($item)) {
+//             $filteredItems[] = $item;
+//         }
+//     }
+//     return $filteredItems;
+// }
+// $filteredBooks = filter($books, function ($book) {
+//     return $book['releaseYear'] >= 2000;
+// });
+
 
 
 require "index.view.php";
